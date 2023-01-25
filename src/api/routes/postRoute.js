60e6ffb5238698150975be5d22a4750a2ce546d6
Fiona Garde <fionaa.garde@gmail.com>
2023-01-25 18:43:23 +0100

    added and updated

---
+++ src/api/routes/postRoute.js
@@ -0,0 +1,9 @@
+module.exports = (server) => {
+    const postController = require("../controllers/postController");
+
+server.route("/posts")
+.get(postController.listAllPosts)
+.post(postController.createAPost);
+
+
+}
